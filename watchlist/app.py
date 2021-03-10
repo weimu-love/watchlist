b77fe062af892ef9b06fe69000398a4217087c8b
Wei Mu <1749765344@qq.com>
2021-03-10 10:51:48 +0800

    Add index page

--- watchlist/app.py
+++ watchlist/app.py
@@ -4,16 +4,30 @@
    Description :
    date：          2021/3/10
 """
-
-from flask import Flask, escape, url_for
+from flask import Flask, render_template
+from flask import escape, url_for
 
 app = Flask(__name__)
+
+name = 'Wei Mu'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 @app.route('/')
 @app.route('/home')
-def hello():
-    return 'welcome to my watchlist'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/img')
