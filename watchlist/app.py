fa89b52c84e1efa398149dad2dd2fd2254c32b93
Wei Mu <1749765344@qq.com>
2021-03-10 12:30:50 +0800

    Create, edit and delete item by form

--- watchlist/app.py
+++ watchlist/app.py
@@ -6,6 +6,7 @@
 """
 from flask import Flask, render_template
 from flask import escape, url_for
+from flask import request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
 import click
@@ -23,6 +24,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'da
 
 # 关闭对模型修改的监控
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# flash() 函数在内部会把消息存储到 Flask 提供的 session 对象里。
+# session 用来在请求间存储数据，它会把数据签名后存储到浏览器的 Cookie 中，所以我们需要设置签名所需的密钥：
+app.config['SECRET_KEY'] = 'dev'
 
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
@@ -78,15 +82,58 @@ def forge():
     click.echo('Done.')
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # 获取表单数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
     # 读取用户记录
     # user = User.query.first()
     # 读取所有电影记录
     movies = Movie.query.all()
     # print(user.name)
     return render_template('index.html', movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
 
 
 @app.errorhandler(404)  # 传入要处理的错误代码
